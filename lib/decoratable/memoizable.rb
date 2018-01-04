85fdbdb41cea865129eef7c582d1b1290b1b57e8
ecin <ecin@copypastel.com>
2018-01-03 16:12:03 -0800

    Fix Memoizable for methods with punctuation.
    We were using instance variables named after the method to cache values.
    However, instance variables can't have punctuation marks (e.g. "!" or
    "?") in their name, thus the problem.

--- lib/decoratable/memoizable.rb
+++ lib/decoratable/memoizable.rb
@@ -3,13 +3,21 @@ require "decoratable"
 module Memoizable
   extend Decoratable
 
-  def memoizable
-    key = :"@#{__decorated_method__.name}_cache"
+  MEMOIZABLE_CACHE_KEY = :"@memoizable_cache"
 
-    if instance_variable_defined?(key)
-      instance_variable_get key
+  def memoizable
+    if instance_variable_defined?(MEMOIZABLE_CACHE_KEY)
+      cache = instance_variable_get(MEMOIZABLE_CACHE_KEY)
     else
-      instance_variable_set key, yield
+      cache = instance_variable_set(MEMOIZABLE_CACHE_KEY, Hash.new)
+    end
+
+    key = __decorated_method__.name.to_sym
+
+    if cache.has_key?(key)
+      cache[key]
+    else
+      cache[key] = yield
     end
   end
 end
--- test/decoratable/memoizable_test.rb
+++ test/decoratable/memoizable_test.rb
@@ -13,16 +13,25 @@ describe Memoizable do
         @count ||= 0
         @count += 1
       end
+
+      memoizable
+      def scream!
+        "BOO!"
+      end
     end
 
     @object = klass.new
   end
 
-  it "memoizes a function's return value" do
+  it "memoizes a method's return value" do
     3.times { @object.call }
 
     # If method was memoized, the counter shouldn't have incremented
     @object.call.must_equal 1
+  end
+
+  it "works with methods that have punctuation marks" do
+    3.times { @object.scream! }
   end
 
 end
